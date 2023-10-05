54b3c89cbaecb1936d424c741001bba6d2e21b75
Wellington-Mariano <wellington.mariano97@hotmail.com>
2023-10-05 13:36:05 +0100

    first commit insertion algorithm

---
+++ src/main/java/com/mycompany/myfirstinsertionalgorithm/MyfirstInsertionAlgorithm.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myfirstinsertionalgorithm;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author wellington-mariano
+ */
+public class MyfirstInsertionAlgorithm {
+
+    public static void main(String[] args) {
+        Scanner mySc = new Scanner(System.in);
+        int[] arr = {10, 5, 8, 9, 4};
+        
+        insertion(arr);
+        
+        System.out.println("Values in correct order: ");
+        
+        for(int arrEl : arr){
+            System.out.printf(" - %d",arrEl);
+            
+        }
+        
+    }
+    
+    public static void insertion(int arr[])
+    {
+        int n = arr.length; // example n is 5
+        
+        for(int i = 1; i < n; i++)
+        {
+            int key = arr[i]; // Second element
+            int j = i - 1; // Very first element
+            
+            while(j >= 0 && arr[j] > key) // Comparison goes up to i - Minor element is sent to beginning of array
+            {
+                arr[j+1] = arr[j]; // The second element is receiving the first element
+                j--; //decrement to stop the loop
+            }
+            
+            arr[j+1] = key;
+        }
+        /*
+        The swap is made by following this: 
+        Key (Current index) value is kept within key
+        If the value of index key -1 is bigger, the current index receives the bigger value
+        and there is now an open space in the array (j - 1) which is then replaced with the smallest value
+        if the smalles value is not yet in the right position, the empty is filled with the bigger element.
+        To summarise, the empty space if filled with the smallest value in the comparisson (key)
+        */
+    }
+}
